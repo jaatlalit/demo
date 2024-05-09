837a17d381eae9a1772fe1baa2d37dbd7373491b
jaatlalit <chaudharylalit025@gmail.com>
2024-05-08 21:26:46 -0700

    third commit

---
+++ new.js
@@ -0,0 +1,4 @@
+console.log("my name is lalit")
+console.log("i am really a fond of singing")
+console.log("i study in Army institute of technology ")
+alert("user is getting registered for this website")
